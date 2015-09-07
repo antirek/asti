33ff46a0783a7a59e9d990490b0b73e5dbca8ebd
antirek <serge.dmitriev@gmail.com>
2015-09-07 10:54:57 +0700

    differents between asterisk versions

--- example/config.js
+++ example/config.js
@@ -1,13 +1,14 @@
 module.exports = {
   ami: {
     port: 5038,
-    host: 'localhost',
-    username: 'admin',
-    password: 'amp111'
+    host: 'p02.mobilon.ru',
+    username: 'amiadmin',
+    password: 'kug09han',
+    version: '1.8'
   },
   web: {
     port: 10000,
     host: '0.0.0.0'
   },
-  key_field: 'interface'
+  key_field: 'agentcalled'
 };
