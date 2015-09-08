19e02cf2ff91a89068ceba0ef3c7f80de57455fa
sergey <serge.dmitriev@gmail.com>
2015-09-08 20:06:12 +0700

    add ioHandler

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ var http = require('http');
 var Joi = require('joi');
 var socket_io = require('socket.io');
 
-
 var ConfigSchema = require('./lib/configSchema');
 var AmiConnection = require('./lib/amiConnection');
 var Router = require('./lib/router');
@@ -12,60 +11,44 @@ var Pool = require('./lib/pool');
 var Client = require('./lib/client');
 var AgentEventsHandler = require('./lib/agentEventsHandler');
 var Call = require('./lib/call');
+var ioHandler = require('./lib/ioHandler');
 
 var Server = function (config) {
 
-    var validate = function (callback) {
-      Joi.validate(config, ConfigSchema, callback);
-    };
+  var validate = function (callback) {
+    Joi.validate(config, ConfigSchema, callback);
+  };
 
-    var init = function () {
+  var init = function () {
+    var amiConnection = new AmiConnection(config.ami);
+    var call = new Call(amiConnection);
+    var router = new Router(call);
 
-      var appendListeners = function (io, pool) {
-        io.on('connection', function (socket) {
-          var client = new Client({socket: socket});
-                
-          client.on('agent:subscribe', function (data) {
-            console.log(data);
-            pool.addClient(data.agent, client);
-          });
+    var pool = new Pool();
+    var handler = new AgentEventsHandler(amiConnection, pool, config.ami.version);
 
-          client.on('agent:unsubscribe', function (data) {
-            pool.removeClient(client);
-          });
+    var app = http.createServer(router);
+    var io = socket_io(app);
+    ioHandler(io, pool);
 
-        });
-      };
+    app.listen(config.web.port, config.web.host);      
+  };
 
-      var amiConnection = new AmiConnection(config.ami);
-      var call = new Call(amiConnection);
-      var router = new Router(call);
+  var start = function () {
+    validate(function (error) {
+      if (error) {
+        console.log('config.js have errors', error);
+      } else {
+        console.log('config.js validated successfully!');
+        init();
+      }
+    });
+  };
 
-      var pool = new Pool();
-      var handler = new AgentEventsHandler(amiConnection, pool, config.ami.version);
+  return {
+    start: start
+  };
 
-
-      var app = http.createServer(router);
-      var io = socket_io(app);
-      app.listen(config.web.port, config.web.host);
-      appendListeners(io, pool);
-
-    };
-
-    var start = function () {
-      validate(function (error) {
-        if (error) {
-          console.log('config.js have errors', error);
-        } else {
-          console.log('config.js validated successfully!');
-          init();
-        }
-      });
-    };
-
-    return {
-      start: start
-    };
 };
 
 module.exports = Server;
